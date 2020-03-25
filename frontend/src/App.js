916d33be39ea29ca0482e48895db9f06ef91569e
Redaloukil <loukil.red@gmail.com>
2020-03-26 00:07:02 +0100

    prettier and eslint integration

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,16 +1,15 @@
-import React, { lazy } from "react";
-import { Route, Switch, BrowserRouter } from "react-router-dom";
-import { Container } from 'semantic-ui-react';
-import { withSuspense } from "./HOC";
-import { Navbar } from './components/navbar';
-import { Footer } from "./components/footer";
-
+import React, { lazy } from "react"
+import { Route, Switch, BrowserRouter } from "react-router-dom"
+import { Container } from "semantic-ui-react"
+import { withSuspense } from "./HOC"
+import { Navbar } from "./components/navbar"
+import { Footer } from "./components/footer"
 
 const Home = lazy(() =>
   import("./containers").then(({ Home }) => ({
     default: Home
   }))
-);
+)
 
 const Routes = () => {
   return (
@@ -19,19 +18,19 @@ const Routes = () => {
         <Route exact path="/" component={withSuspense(Home)} />
       </Switch>
     </BrowserRouter>
-  );
-};
+  )
+}
 
 const App = () => {
   return (
     <>
-      <Navbar/>
+      <Navbar />
       <Container>
         <Routes />
       </Container>
-      <Footer/>
+      <Footer />
     </>
   )
-};
+}
 
-export default App;
+export default App
