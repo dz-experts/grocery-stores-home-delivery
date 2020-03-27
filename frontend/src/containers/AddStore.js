62a8511f90742ba2966b2aca13892b85b19cb5a5
Bentaimia Haddadi <bentaimia.haddadi@signify.com>
2020-03-27 20:59:44 +0100

    feat: add new store from

--- frontend/src/containers/AddStore.js
+++ frontend/src/containers/AddStore.js
@@ -1,11 +1,13 @@
 import React from "react";
 import { useTranslation } from "react-i18next";
+import { StoreForm } from "../components/form";
 
 const AddStore = () => {
   const { t } = useTranslation();
   return (
     <div>
       <h2>{t("addNewStore")}</h2>
+      <StoreForm />
     </div>
   );
 };
--- frontend/src/containers/Home.js
+++ frontend/src/containers/Home.js
@@ -1,37 +1,34 @@
-import React , { useEffect }from "react";
-import styled from 'styled-components';
-import { MapContainer } from '../components/map';
-import {getGeoLocation} from '../utils/geolocation';
-
+import React, { useEffect } from "react"
+import styled from "styled-components"
+import { MapContainer } from "../components/map"
+import { getGeoLocation } from "../utils"
 
 const Home = () => {
   const state = {
-    position:{
-    
-    }
+    position: {},
   }
-  useEffect(()=>{
-      getGeoLocation()
-        .then(pos => {
-          state.position.latitude = pos.coords.latitude;
-          state.position.longitude = pos.coords.longitude;
-        })
-        .catch(err => {
-          console.log(err);
-        })
+  useEffect(() => {
+    getGeoLocation()
+      .then((pos) => {
+        state.position.latitude = pos.coords.latitude
+        state.position.longitude = pos.coords.longitude
+      })
+      .catch((err) => {
+        console.log(err)
+      })
   })
   return (
     <div>
       <Component>
-        <MapContainer position={state.position}/>
+        <MapContainer position={state.position} />
       </Component>
     </div>
-  );
-};
+  )
+}
 
 const Component = styled.section`
   height: 400px;
   width: 100%;
-`;
+`
 
-export { Home };
+export { Home }
